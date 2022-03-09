45d08af05dbc67d2221b6fa152a6bdd77f081ba8
Vikram Raja <vvraja2301@gmail.com>
2022-03-09 06:01:03 +0530

    product validation middleware bug cleared

--- schema.js
+++ schema.js
@@ -7,14 +7,11 @@ module.exports.productSchema = Joi.object({
         model: Joi.string().required(),
         price: Joi.number().required().min(0),
         color: Joi.string().required(),
-        dial: Joi.string().required(),
+        description: Joi.string().required(),
         image: Joi.string().required(),
-        waterproof: Joi.string().required(),
-        caseshape: Joi.string().required(),
+        gender: Joi.string().required(),
         warranty: Joi.number().required().min(0),
         stocks: Joi.number().required().min(10),
-        description: Joi.string().required(),
-        gender: Joi.string().required(),
     }).required()
 })
 
